539dfc94d2058fc45d8047a0d5b8cbbc8e4a66dd
Oskar Williams <Oskar.Williams@softwire.com>
2019-07-22 11:38:23 +0100

    Version 1.3, added rook move

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,21 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        const locations = [];
+        for (let i = 0; i<8;i++) {
+            let newLocationRow;
+            let newLocationCol;
+            if (i !== location.row){
+                newLocationRow  = new Square(i, location.col);
+                locations.push(newLocationRow);
+            }
+            if (i !== location.col) {
+                newLocationCol  = new Square(location.row, i);
+                locations.push(newLocationCol);
+            }     
+        }
+
+        return locations;
     }
 }
